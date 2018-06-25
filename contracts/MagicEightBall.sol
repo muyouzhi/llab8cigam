91f73721fe9a02a1a95501b49f7f65b21bf41956
Di Mu <dimu@dis-air-2.lan>
2018-06-25 11:26:55 -0700

    Add comments

--- contracts/MagicEightBall.sol
+++ contracts/MagicEightBall.sol
@@ -2,8 +2,9 @@ pragma solidity ^0.4.23;
 
 import "./MagicToken.sol";
 
-// Controlls the issuance of MagicToken
 
+// Asker pays a token to ask questions
+// User can answer a question assigned by the system to get a token 
 contract MagicEightBall {
 	struct Question {
 		string questionText;
@@ -14,7 +15,10 @@ contract MagicEightBall {
 	MagicToken public tokenContract;
 	uint currentId;
 	uint lastUnansweredId;
+
+	// Store questions by id
 	mapping(uint => Question) questions;
+	// List of user's questions
 	mapping(address => uint[]) userQuestions;
 
 	constructor(address tokenAddress) public {
@@ -22,6 +26,8 @@ contract MagicEightBall {
 		lastUnansweredId = 0;
 	}
 
+	// Ask a question of `text`
+	// @text question text
 	function ask(string text) public returns (bool){
 		require(notEmpty(text));
 		if (tokenContract.balanceOf(msg.sender) >= 1) {
@@ -33,6 +39,7 @@ contract MagicEightBall {
 		return true;
 	}
 
+	// Get question id of the question to be answered
 	function getNextQuestionIdToAnswer() public returns (uint) {
 		while (questions[lastUnansweredId].isAnswered) {
 			lastUnansweredId = lastUnansweredId + 1;
@@ -40,22 +47,27 @@ contract MagicEightBall {
 		return lastUnansweredId;
 	}
 
+	//  Get the question text of an unanswered question with id 'id'
 	function getUnansweredQuestion(uint id) public view returns (string) {
 		require(!questions[id].isAnswered);
 		Question q = questions[id];
 		return q.questionText;
 	}
 
+	// Get the answer of the question with id `id`
 	function getAnswerForQuestion(uint id) public view returns (string) {
 		require(questions[id].isAnswered);
 		Question q = questions[id];
 		return q.answerText;
 	}
 
+	// Get a list of questions
 	function getMyQuestionIds() public view returns (uint[]) {
 		return userQuestions[msg.sender];
 	}
 
+
+	// answer the question of id `questionId` with answer `text`
 	function answer(uint questionId, string text) public returns (bool){
 		require(notEmpty(text));
 		require(!notEmpty(questions[questionId].answerText));
@@ -68,6 +80,7 @@ contract MagicEightBall {
 		return true;
 	}
 
+	// Check whether a string is empty
     function notEmpty(string text) public pure returns (bool) {
         bytes memory _content = bytes(text);
         return (_content.length != 0);
